3286eced6fcd8dd84d2bf41daa995991055ece82
Otávio Queiroz <otavioqs16@gmail.com>
2021-04-17 00:59:08 -0300

    initial commit

---
+++ src/main/java/com/digitalinnotavionone/heroesapi/constants/HeroesConstant.java
@@ -0,0 +1,8 @@
+package com.digitalinnotavionone.heroesapi.constants;
+
+public class HeroesConstant {
+    public static final String HEROES_ENDPOINT_LOCAL="/heroes";
+    public static final String DYNAMO_ENDPOINT="http://localhost:8000";
+    public static final String DYNAMO_REGION="us-east-1";
+
+}
